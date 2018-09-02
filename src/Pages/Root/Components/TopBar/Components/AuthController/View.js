c8d692b65eb622f9cb68656961bcced73d26a0c6
soulike <316413310@qq.com>
2018-09-02 18:07:22 +0800

    改进在移动端的排版表现

--- src/Pages/Root/Components/TopBar/Components/AuthController/View.js
+++ src/Pages/Root/Components/TopBar/Components/AuthController/View.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {View as TopBarLink} from '../TopBarLink';
 import {Actions} from '../../../../../../Components/AuthProcessor';
 import * as solidIcons from '@fortawesome/free-solid-svg-icons';
+import './AuthController.css';
 
 class AuthController extends Component
 {
@@ -10,29 +11,26 @@ class AuthController extends Component
     {
         const {hasLoggedIn, onClickExitLink} = this.props;
         // 如果没有登陆，就显示登陆按钮
-        if (!hasLoggedIn)
-        {
-            return (
-                <div>
-                    <TopBarLink to={'/login'} text={'登录'} iconStyle={solidIcons.faUser}/>
-                </div>
-            );
-        }
-        // 如果登陆了，就显示功能按钮
-        else
-        {
-            return (
-                <div>
-                    <TopBarLink to={'/dynamic'} text={'动态'} iconStyle={solidIcons.faInfoCircle}/>
-                    <TopBarLink to={'/articleEditor'} text={'写文章'} iconStyle={solidIcons.faPenSquare}/>
-                    <TopBarLink to={'/options'} text={'设置'} iconStyle={solidIcons.faCogs}/>
-                    <TopBarLink to={'#'}
-                                text={'退出登录'}
-                                iconStyle={solidIcons.faSignOutAlt}
-                                onClick={onClickExitLink}/>
-                </div>
-            );
-        }
+        return (
+            <div className={'AuthController'}>
+                {hasLoggedIn ?
+                    (
+                        <div>
+                            <TopBarLink to={'/dynamic'} text={'动态'} iconStyle={solidIcons.faInfoCircle}/>
+                            <TopBarLink to={'/articleEditor'} text={'写文章'} iconStyle={solidIcons.faPenSquare}/>
+                            <TopBarLink to={'/options'} text={'设置'} iconStyle={solidIcons.faCogs}/>
+                            <TopBarLink to={'#'}
+                                        text={'退出登录'}
+                                        iconStyle={solidIcons.faSignOutAlt}
+                                        onClick={onClickExitLink}/>
+                        </div>
+                    ) :
+                    (
+                        <TopBarLink to={'/login'} text={'登录'} iconStyle={solidIcons.faUser}/>
+                    )
+                }
+            </div>
+        );
     }
 }
 
