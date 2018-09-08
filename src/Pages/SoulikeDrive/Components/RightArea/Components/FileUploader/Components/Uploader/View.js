c9a9c92b3eadb3fb8d4f5a7050b55ca1059dd81b
Soulike <316413310@qq.com>
2018-09-08 10:32:57 +0800

    修复bug

--- src/Pages/SoulikeDrive/Components/RightArea/Components/FileUploader/Components/Uploader/View.js
+++ src/Pages/SoulikeDrive/Components/RightArea/Components/FileUploader/Components/Uploader/View.js
@@ -52,7 +52,8 @@ class Uploader extends Component
                 {
                     this.setState({updateProgress: event.loaded / event.total});
                 }
-            }
+            },
+            withCredentials: true
         })
             .then(res =>
             {
